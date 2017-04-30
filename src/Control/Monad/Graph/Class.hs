9d48cba4d0381af0849aeef248f4abb3b72e7603
Aaron Friel <mayreply@aaronfriel.com>
2017-04-30 03:14:15 -0500

    Move indexed out of normal build dependencies.

--- src/Control/Monad/Graph/Class.hs
+++ src/Control/Monad/Graph/Class.hs
@@ -13,7 +13,7 @@ module Control.Monad.Graph.Class where
 
 import Data.Kind (Constraint, type (*))
 import Control.Monad
-import Control.Monad.Indexed
+-- import Control.Monad.Indexed
 import Data.Type.Equality
 import GHC.Base (Any)
 
@@ -169,34 +169,3 @@ instance Monad m => KMonad (IxEff m) where
 
 instance MonadPlus m => KMonadFail (IxEff m) where
     kfail _ = IxEff $ mzero
-
--- Wrapped IxMonad from Control.Monad.Indexed
-newtype WrappedIx (m :: k -> k -> * -> *) (p :: CatArr k k) a = WrappedIx { unWix :: m (FstArr p) (SndArr p) a }
-
-instance KEffect (WrappedIx m) where
-    type Unit (WrappedIx m) = 'CatId
-    type Inv  (WrappedIx m) i j = (SndArr i ~~ FstArr j)
-    type Plus (WrappedIx m) i j  = 'CArrow (FstArr i) (SndArr j)  
-
-instance Fmappable (WrappedIx m)
-instance Applyable (WrappedIx m) where
-    type Then (WrappedIx m) i j = Plus (WrappedIx m) i j
-    type But (WrappedIx m) i j = Plus (WrappedIx m) i j
-instance Bindable (WrappedIx m)
-
-instance IxPointed m => KPointed (WrappedIx m) where
-    kreturn = WrappedIx . ireturn
-
-instance IxFunctor m => KFunctor (WrappedIx m) where
-    kmap f = WrappedIx . imap f . unWix
-
-instance IxApplicative m => KApplicative (WrappedIx m) where
-    kap (WrappedIx m) (WrappedIx k) = WrappedIx $ m `iap` k
-    kthen (WrappedIx m) (WrappedIx k) = WrappedIx $ (imap . const) id m `iap` k
-    kbut (WrappedIx m) (WrappedIx k) = WrappedIx $ ireturn const `iap` m `iap` k
-
-instance IxMonad m => KMonad (WrappedIx m) where
-    kbind (WrappedIx m) k = WrappedIx $ (unWix . k) `ibind` m
-
-instance IxMonadZero m => KMonadFail (WrappedIx m) where
-    kfail _ = WrappedIx $ imzero
