97307be0d38f83aa337d7bd5b74179a5380f5137
Aaron Friel <mayreply@aaronfriel.com>
2017-05-03 15:23:23 -0500

    Add graphted dependency, update Class.hs for example

--- src/Control/Monad/Graph/Class.hs
+++ src/Control/Monad/Graph/Class.hs
@@ -3,57 +3,37 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
--- Makes for nicer type families where Plus is reused.
+-- Makes for nicer type families where Combine is reused.
 {-# LANGUAGE UndecidableInstances #-}
 
 {-# LANGUAGE StrictData #-}
 
+{-# LANGUAGE Safe #-}
+
 module Control.Monad.Graph.Class where
 
 import Data.Kind (Constraint, type (*))
-import Control.Monad
--- import Control.Monad.Indexed
-import Data.Type.Equality
-import GHC.Base (Any)
+import Control.Monad (MonadPlus, mzero)
+import Data.Type.Equality (type (~~))
 
 class KEffect (f :: k -> * -> *) where
     type Unit f :: k
     type Inv f (i :: k) (j :: k) :: Constraint
-    type family Plus f (i :: k) (j :: k) :: k
-
-class Fmappable (f :: k -> * -> *) where
-    type family Fmap (f :: k -> * -> *) (i :: k) :: k
-    type instance Fmap f i = i
-
-    type family Fconst f (i :: k) :: k
-    type instance Fconst f i = Fmap f i
-
-class KEffect f => Applyable (f :: k -> * -> *) where
-    type family Apply (f :: k -> * -> *) (i :: k) (j :: k) :: k
-    type instance Apply f i j = Plus f i j
-
-    type family Then f (i :: k) (j :: k) :: k
-    type instance Then f i j = Apply f (Fconst f i) j
-
-    type family But f (i :: k) (j :: k) :: k
-    type instance But f i j = Apply f (Apply f (Unit f) i) j
-
-class KEffect f => Bindable (f :: k -> * -> *) where
-    type family Bind (f :: k -> * -> *) (i :: k) (j :: k) :: k
-    type instance Bind f i j = Plus f i j
-
-    type family Join (f :: k -> * -> *) (i :: k) (j :: k) :: k
-    type instance Join f i j = Bind f i j
-
-class KEffect f => Fallible (f :: k -> * -> *) where
-    type family Fail (f :: k -> * -> *) :: k
-    type instance Fail f = Unit f
+    type family Combine f (i :: k) (j :: k) :: k
 
 class KEffect f => KPointed (f :: k -> * -> *) where
     kreturn :: a -> f (Unit f) a
 
 class KEffect f => KFunctor (f :: k -> * -> *) where
+    type family Fmap f (i :: k) :: k
+    type instance Fmap f i = i
+
+    type family Fconst f (i :: k) :: k
+    type instance Fconst f i = Fmap f i
+
     kmap :: (a -> b) -> f i a -> f (Fmap f i) b
 
     {-# INLINABLE kconst #-}
@@ -62,6 +42,15 @@ class KEffect f => KFunctor (f :: k -> * -> *) where
     kconst = kmap . const
 
 class (KFunctor f, KPointed f) => KApplicative (f :: k -> * -> *) where
+    type family Apply f (i :: k) (j :: k) :: k
+    type instance Apply f i j = Combine f i j
+
+    type family Then f (i :: k) (j :: k) :: k
+    type instance Then f i j = Apply f (Fconst f i) j
+
+    type family But f (i :: k) (j :: k) :: k
+    type instance But f i j = Apply f (Apply f (Unit f) i) j
+
     kap :: Inv f i j => f i (a -> b) -> f j a -> f (Apply f i j) b
 
     -- *>
@@ -79,6 +68,12 @@ class (KFunctor f, KPointed f) => KApplicative (f :: k -> * -> *) where
     kbut a b = kreturn const `kap` a `kap` b
 
 class KApplicative f => KMonad (f :: k -> * -> *) where
+    type family Bind f (i :: k) (j :: k) :: k
+    type instance Bind f i j = Combine f i j
+
+    type family Join f (i :: k) (j :: k) :: k
+    type instance Join f i j = Bind f i j
+
     kbind :: Inv f i j => f i a -> (a -> f j b) -> f (Bind f i j) b
 
     {-# INLINE kjoin #-}
@@ -87,7 +82,35 @@ class KApplicative f => KMonad (f :: k -> * -> *) where
     kjoin x = x `kbind` id
 
 class KMonad f => KMonadFail (f :: k -> * -> *) where
+    type family Fail f :: k
+    type instance Fail f = Unit f
+
     kfail :: String -> f (Fail f) a
+
+    default kfail :: (KMonadZero f, Unit f ~ Fail f) => String -> f (Fail f) a
+    kfail _ = kzero
+
+class KMonad f => KMonadZero (f :: k -> * -> *) where
+    kzero :: f (Unit f) a
+
+-- Should satisfy left distribution, use the operator <+>
+class KMonad f => KMonadPlus (f :: k -> * -> *) where
+    type family Plus f (i :: k) (j :: k) :: k
+    type instance Plus f i j = Combine f i j
+
+    kplus :: Inv f i j => f i a -> f j a -> f (Plus f i j) a
+
+-- Should satisfy left catch, use the operator <|>
+class KMonad f => KMonadOr (f :: k -> * -> *) where
+    type family Or f (i :: k) (j :: k) :: k
+    type instance Or f i j = Combine f i j
+
+    korelse :: Inv f i j => f i a -> f j a -> f (Or f i j) a
+
+class KMonad f => KMonadFix (f :: k -> * -> *) where
+    type family Fix f (i :: k) :: k
+
+    kfix :: (a -> f i a) -> f (Fix f i) a
 
 -- Wrapping a non-indexed type: 
 newtype WrappedM (m :: * -> *) (p :: ()) a where
@@ -100,13 +123,9 @@ liftWM :: m a -> WrappedM m '() a
 liftWM = WrappedM
 
 instance KEffect (WrappedM m :: () -> * -> *) where
-    type Unit (WrappedM m) = '()
-    type Inv  (WrappedM m) i j = (i ~ '(), j ~ '())
-    type Plus (WrappedM m) i j = '()
-
-instance Fmappable (WrappedM m)
-instance Applyable (WrappedM m)
-instance Bindable (WrappedM m)
+    type Unit (WrappedM _) = '()
+    type Inv  (WrappedM _) i j = (i ~ '(), j ~ '())
+    type Combine (WrappedM _) _ _ = '()
 
 instance Applicative m => KPointed (WrappedM m) where
     kreturn = WrappedM . pure
@@ -126,46 +145,41 @@ instance Monad m => KMonadFail (WrappedM m) where
     kfail = WrappedM . fail
 
 -- Wrapping an indexed type with two phantom parameters, parameters form a category.
-newtype IxEff (m :: * -> *) (p :: CatArr i j) (a :: *) = IxEff { unIx :: m a }
+newtype WrappedIx (m :: * -> *) (p :: CatArr i j) (a :: *) = WrappedIx { unIx :: m a }
 
 data CatArr a b where
     CatId :: CatArr a b
     CArrow  :: a -> b -> CatArr a b
 
-instance KEffect (IxEff m :: CatArr i j -> * -> *) where
-    type Unit (IxEff m) = 'CatId
-    type Inv  (IxEff m) ('CArrow a b) ('CArrow c d) = b ~~ c
-    type Plus (IxEff m) ('CatId     ) ('CatId     ) = 'CatId 
-    type Plus (IxEff m) ('CArrow a b) ('CArrow c d) = 'CArrow a d
-    type Plus (IxEff m) ('CArrow a b) ('CatId     ) = 'CArrow a b
-    type Plus (IxEff m) ('CatId     ) ('CArrow c d) = 'CArrow c d
-
-instance Fmappable (IxEff m)
-instance Applyable (IxEff m) where
-    type But (IxEff m) i j = Plus (IxEff m) i j
-instance Bindable (IxEff m)
+instance KEffect (WrappedIx m :: CatArr i j -> * -> *) where
+    type Unit (WrappedIx _) = 'CatId
+    type Inv  (WrappedIx _) ('CArrow _ b) ('CArrow c _) = b ~~ c
+    type Combine (WrappedIx _) ('CatId     ) ('CatId     ) = 'CatId 
+    type Combine (WrappedIx _) ('CArrow a _) ('CArrow _ d) = 'CArrow a d
+    type Combine (WrappedIx _) ('CArrow a b) ('CatId     ) = 'CArrow a b
+    type Combine (WrappedIx _) ('CatId     ) ('CArrow c d) = 'CArrow c d
 
 type family FstArr (p :: CatArr i j) where
-    FstArr ('CArrow a b) = a
-    FstArr ('CatId :: CatArr i j) = (Any :: i)
+    FstArr ('CArrow a _) = a
 
 type family SndArr (p :: CatArr i j) where
-    SndArr ('CArrow a b) = b
-    SndArr ('CatId :: CatArr i j) = (Any :: j)
+    SndArr ('CArrow _ b) = b
 
-instance Applicative m => KPointed (IxEff m) where
-    kreturn = IxEff . pure
+instance Applicative m => KPointed (WrappedIx m) where
+    kreturn = WrappedIx . pure
 
-instance Functor m => KFunctor (IxEff m) where
-    kmap f = IxEff . fmap f . unIx
+instance Functor m => KFunctor (WrappedIx m) where
+    kmap f = WrappedIx . fmap f . unIx
 
-instance Applicative m => KApplicative (IxEff m) where
-    kap (IxEff m) (IxEff k) = IxEff $ m <*> k
-    kthen (IxEff m) (IxEff k) = IxEff $ (m *> k)
-    kbut (IxEff m) (IxEff k) = IxEff $ (m <* k)
+instance Applicative m => KApplicative (WrappedIx m) where
+    type But (WrappedIx m) i j = Combine (WrappedIx m) i j
 
-instance Monad m => KMonad (IxEff m) where
-    kbind (IxEff m) k = IxEff $ m >>= unIx . k
+    kap (WrappedIx m) (WrappedIx k) = WrappedIx $ m <*> k
+    kthen (WrappedIx m) (WrappedIx k) = WrappedIx $ (m *> k)
+    kbut (WrappedIx m) (WrappedIx k) = WrappedIx $ (m <* k)
 
-instance MonadPlus m => KMonadFail (IxEff m) where
-    kfail _ = IxEff $ mzero
+instance Monad m => KMonad (WrappedIx m) where
+    kbind (WrappedIx m) k = WrappedIx $ m >>= unIx . k
+
+instance MonadPlus m => KMonadFail (WrappedIx m) where
+    kfail _ = WrappedIx $ mzero
